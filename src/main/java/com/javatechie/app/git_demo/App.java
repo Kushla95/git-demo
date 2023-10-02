0fc303c2aa2aeb89b5cdfd167b0c0a3393c712d4
Kushla95 <das.kushal95@gmail.com>
2023-10-02 13:02:36 +0530

    added if condition to check the data

--- src/main/java/com/javatechie/app/git_demo/App.java
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -4,8 +4,16 @@ package com.javatechie.app.git_demo;
  * Hello world!
  *
  */
+
+
 public class App 
 {
+	public void addData(String data) {
+		System.out.println("data ="+data);
+		if(data.equals("java techie")) {
+			System.out.println("Welcome");
+		}
+	}	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
